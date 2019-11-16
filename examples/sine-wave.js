d9dbc53ed1c964293e9d35704d08598cf1375d14
rubyrubenstahl <ruby@ruby-lighting.com>
2019-11-16 17:51:46 -0500

    Add sine wave sample

--- examples/sine-wave.js
+++ examples/sine-wave.js
@@ -3,7 +3,6 @@ const mk2 = new MK2("COM4");
 
 function update() {
   const ms = new Date().getTime();
-
   const level = 128 + Math.sin(ms / 1000) * 100;
   const dmxData = Array(512).fill(level);
   mk2.writeDmxData(dmxData, 1);
